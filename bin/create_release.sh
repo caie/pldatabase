56b145813ae76e1655a7738ca9da9d946ef7f847
landon.j.fuller <landon.j.fuller@3603cd71-484c-0410-acc4-09e5374cfb26>
2008-11-24 03:33:09 +0000

    - Ensure that all paths are safely "" quoted. - Update all comments to use proper english capitalization.

--- bin/create_release.sh
+++ bin/create_release.sh
@@ -1,39 +1,40 @@
 #!/bin/sh
 
+PROJECT_NAME="pldatabase"
+
+# Parse script arguments
 print_usage () {
     echo "Usage: $0 <version> <source branch>"
 }
 
-# check the arguments
 VERSION=$1
-if [ -z $VERSION ]; then
-    print_usage
-    exit 1
-fi
-
 SOURCE=$2
-if [ -z $SOURCE ]; then
+
+if [ -z "$VERSION" ] || [ -z "$SOURCE" ]; then
     print_usage
     exit 1
 fi
 
-# be sure the checkout is up to date
-svn up
-if [ $? -gt 0 ]; then
-    echo "SVN updating failed, aborting tagging process."
+# Xcode paths
+XCODE_BASE=`xcode-select -print-path`
+DOCSET_UTIL="${XCODE_BASE}/usr/bin/docsetutil"
+
+if [ -z "$XCODE_BASE" ]; then
+    echo "Could not determine Xcode installation path"
     exit 1
 fi
 
-TAG="tags/pldatabase-$VERSION"
+# Determine the tag path
+TAG="tags/${PROJECT_NAME}-$VERSION"
 
-# verify the tag is new
-if [ -d $TAG ]; then
+# Verify the tag is new
+if [ -d "$TAG" ]; then
     echo "Tag $TAG already exists, aborting."
     exit 1
 fi
 
-# create the tag
-svn cp $SOURCE $TAG 
+# Create the tag
+svn cp "$SOURCE" "$TAG"
 if [ $? -gt 0 ]; then
     echo "Creating the tag failed, aborting tagging process."
     exit 1
@@ -41,20 +42,20 @@ fi
 
 # Build the documentation
 pushd .
-cd $TAG
+cd "$TAG"
 doxygen
 
-# validate the build worked
+# Validate the build worked
 if [ $? -gt 0 ]; then
     echo "Build failed, aborting tagging process."
     exit 1
 fi
 
-# back down to the root
+# Back down to the root
 popd
 
-# add the docs to the tag since we need these to be served on google code
-svn add $TAG/docs
+# Add the docs to the tag
+svn add "$TAG/docs"
 
-# fix up the svn mimetypes for the html [needed by google code]
-find $TAG/docs -name \*.html -exec svn propset svn:mime-type text/html {} \;
+# Set the svn mimetypes for the documentation HTML [needed by google code]
+find "$TAG/docs" -name \*.html -exec svn propset svn:mime-type text/html {} \;
